c1969dc0d24b018a68fa168ba4a4f5a97dd84481
shruti <shrutiirpate01@gmail.com>
2026-02-11 20:13:07 +0530

    Added Datatypes class

---
+++ Day01/Datatype.java
@@ -0,0 +1,10 @@
+package Day01;
+
+import com.sun.nio.sctp.SctpSocketOption;
+
+public class Datatype {
+    public static void main(String[] args) {
+        System.out.println("This is a Datatype class");
+    }
+    
+}
