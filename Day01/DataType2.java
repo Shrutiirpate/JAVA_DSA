ed44566c6d17934fe7554a10c9e4ae66febccd60
shruti <shrutiirpate01@gmail.com>
2026-02-12 22:40:38 +0530

    Added Basics of Datatypes

---
+++ Day01/DataType2.java
@@ -0,0 +1,21 @@
+package Day01;
+
+public class DataType2 {
+
+    public static void main(String[] args) {
+
+        byte age = 22;
+        int rollNo = 101;
+        long mobile = 9876543210L;
+        float percentage = 85.5f;
+        double height = 5.4;
+        char grade = 'A';
+
+        System.out.println("Age: " + age);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Mobile: " + mobile);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+    }
+}
--- Day01/Datatype.java
+++ Day01/Datatype.java
@@ -5,6 +5,7 @@ import com.sun.nio.sctp.SctpSocketOption;
 public class Datatype {
     public static void main(String[] args) {
         System.out.println("This is a Datatype class");
+
     }
     
 }
