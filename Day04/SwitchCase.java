86b1f070e6262228899d7ab2422075a3d541c288
shruti <shrutiirpate01@gmail.com>
2026-02-20 23:09:54 +0530

    Solved Switchcase Questions and learn how to use if else and switch together.

--- Day04/SwitchCase.java
+++ Day04/SwitchCase.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class SwitchCase {
 
-    public static void main(String[] args) {
+    public static void main(String[]args) {
 
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter language: ");
@@ -30,41 +30,41 @@ public class SwitchCase {
 //        }
         //----------------------------------------------------------------
 
-        int x = 3;
-        switch (x){
-            case 1 :
-                System.out.println("one");
-                break;
-            case 2:
-                System.out.println("Two");
-                break;
-            case 3:
-                System.out.println("Three");
-                break;
-            case 4:
-                System.out.println("Four");
-                break;
-            case 5:
-                System.out.println("Five");
-                break;
-            case 6:
-                System.out.println("Six");
-                break;
-            case 7:
-                System.out.println("Seven");
-                break;
-            case 8:
-                System.out.println("Eight");
-                break;
-            case 9:
-                System.out.println("Nine");
-                break;
-            case 10:
-                System.out.println("Ten");
-                break;
-            default:
-                System.out.println("Invalid number");
-        }
+//        int x = 3;
+//        switch (x){
+//            case 1 :
+//                System.out.println("one");
+//                break;
+//            case 2:
+//                System.out.println("Two");
+//                break;
+//            case 3:
+//                System.out.println("Three");
+//                break;
+//            case 4:
+//                System.out.println("Four");
+//                break;
+//            case 5:
+//                System.out.println("Five");
+//                break;
+//            case 6:
+//                System.out.println("Six");
+//                break;
+//            case 7:
+//                System.out.println("Seven");
+//                break;
+//            case 8:
+//                System.out.println("Eight");
+//                break;
+//            case 9:
+//                System.out.println("Nine");
+//                break;
+//            case 10:
+//                System.out.println("Ten");
+//                break;
+//            default:
+//                System.out.println("Invalid number");
+//        }
         //-----------------------------------------------------------------
         //QUESTION1:-Simple Calculator: Create a menu-driven program that asks the user to input
         // two numbers and an operator character (+, -, *, /). Use a switch statement to perform the chosen arithmetic
