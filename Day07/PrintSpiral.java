52371750bcab6ec18fc06e9e091262418070f376
shruti <shrutiirpate01@gmail.com>
2026-03-29 23:34:31 +0530

    Array - Prefix sum

--- Day07/PrintSpiral.java
+++ Day07/PrintSpiral.java
@@ -71,6 +71,64 @@ import java.util.Scanner;
 //------------------------------------------------------------------------------------------------------------------------------------------------------
 //given a postive integer
 
+//public class PrintSpiral {
+//    static void Print(int[][] matrix ){
+//        for(int i = 0; i < matrix.length; i++){
+//            for(int j = 0; j < matrix[i].length; j++){
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println(" ");
+//        }
+//
+//    }
+//    static int[][] GenerateSpiralMatrix(int n) {
+//        int[][] matrix = new int[n][n];
+//        int topRow = 0, bottomRow = n - 1, leftCol = 0, rightCol = n - 1;
+//        int Curr = 1;
+//
+//        while (Curr <= n * n) {
+//            //topRow -> leftCol to rightCol
+//            for (int j = leftCol; j <= rightCol && Curr <= n * n; j++) {
+//                matrix[topRow][j] = Curr; // Cur++ POST increment
+//                Curr++;
+//            }
+//            topRow++;
+//
+//            //rightCol -> topRow to bottomRow
+//            for (int i = topRow; i <= bottomRow && Curr <= n * n; i++) {
+//                matrix[i][rightCol] = Curr;
+//                Curr++;
+//            }
+//            rightCol--;
+//
+//            //bottomRow -> rightCol to leftCol
+//            for (int j = rightCol; j >= leftCol && Curr <= n * n; j--) {
+//                matrix[bottomRow][j] = Curr;
+//                Curr++;
+//            }
+//            bottomRow--;
+//
+//            // leftCol -> bottomRow to topRow
+//            for (int i = bottomRow; i >= topRow && Curr <= n * n; i--) {
+//                matrix[i][leftCol] = Curr;
+//                Curr++;
+//            }
+//            leftCol++;
+//        }
+//        return matrix;
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter values");
+//        int n = sc.nextInt();
+//
+//        int [][] matrix = GenerateSpiralMatrix(n);
+//        Print(matrix);
+//    }
+//}
+//--------------------------------------------------------------------------------------------------------------------
+//assignment(incomplete)
 public class PrintSpiral {
     static void Print(int[][] matrix ){
         for(int i = 0; i < matrix.length; i++){
@@ -83,8 +141,8 @@ public class PrintSpiral {
     }
     static int[][] GenerateSpiralMatrix(int n) {
         int[][] matrix = new int[n][n];
-        int topRow = 0, bottomRow = n - 1, leftCol = 0, rightCol = n - 1;
-        int Curr = 1;
+        int topRow = n-1, bottomRow = n, leftCol = n-1, rightCol = n;
+        int Curr =  1;
 
         while (Curr <= n * n) {
             //topRow -> leftCol to rightCol
