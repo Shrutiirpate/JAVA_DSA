fae5593658ef5b9b29fea3ae82a22a22a0a838be
shruti <shrutiirpate01@gmail.com>
2026-03-30 23:18:16 +0530

    Array - Prefix sum

--- Day07/RectangleSum.java
+++ Day07/RectangleSum.java
@@ -39,12 +39,13 @@ import java.util.Scanner;
 //}
 //Second approch
 public class RectangleSum {
-    static int findSum(int[][]matrix, int l1, int l2, int r1, int r2){
+    //First Approch
+    static int findSum(int[][]matrix, int r1, int r2, int c1, int c2){
         int sum = 0;
 
-        for(int i = l1; i <= l2; i++){
-            for(int j = r1; j <=r2; j++){
-                sum = sum + matrix[i][j];
+        for(int i = r1; i <= r2; i++){
+            for(int j = c1; j <= c2; j++){
+                sum += matrix[i][j];
             }
         }
         return sum;
@@ -60,16 +61,16 @@ public class RectangleSum {
             }
         }
     }
-    static int findSum2(int[][]matrix, int l1, int l2, int r1, int r2){
+    static int findSum2(int[][]matrix, int r1, int r2, int c1, int c2){
         int sum = 0;
         findPrefixSumofMatrix(matrix);
 
-        for(int i = l1; i <= l2; i++ ){
+        for(int i = r1; i <= r2; i++ ){
             //r1 to r2 sum for row i
-            if(r1 >= 1)
-                sum += matrix[i][r2] - matrix[i][r1 - 1];
+            if(c1 >= 1)
+                sum += matrix[i][c2] - matrix[i][c1 - 1];
             else
-                sum += matrix[i][r2];
+                sum += matrix[i][c2];
         }
         return sum;
     }
@@ -88,13 +89,13 @@ public class RectangleSum {
                 matrix[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Enter Reactangle boundries l1, l2 ,r1 ,r2 ");
-        int l1 = sc.nextInt();
-        int l2 = sc.nextInt();
+        System.out.println("Enter Reactangle boundries r1, r2 ,c1 ,c2 ");
         int r1 = sc.nextInt();
         int r2 = sc.nextInt();
+        int c1 = sc.nextInt();
+        int c2 = sc.nextInt();
 
-        System.out.println("Reatangle sum: " + findSum(matrix, l1, l2,r1,r2));
-        System.out.println("Reatangle sum: " + findSum2(matrix, l1, l2,r1,r2));
+        System.out.println("Reatangle sum: " + findSum(matrix, r1, r2,c1,c2));
+        System.out.println("Reatangle sum: " + findSum2(matrix,r1, r2,c1,c2));
     }
 }
